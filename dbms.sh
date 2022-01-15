53e2278a71a8d4587f1966f3318e2eb771577a55
BURING-COMA <xmanhycal@gmail.com>
2022-01-15 09:50:28 +0200

    create dbms.sh: main script file

---
+++ dbms.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+
+# some config   
+
+
+# components
+
+
+# main menu 
+    # into to program msg
+    # general actions
+        # create db
+        # drop db 
+        # list dbs
+        # connet to db
+        # exit
+
+
+# db menu
+    # header msg 
+    # create table  
+    # drop 
+    # list  
+    # update  
+    # insert  
+    # delete from table 
+    # disconn from this db and back to main menu
+    # exit dbms
+
